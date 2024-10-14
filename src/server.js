3be7e32149a9f8123950119e809a68a7175bab22
Paul Lockyer <plockyer@googlemail.com>
2024-10-14 11:30:53 +0100

    enabled logging for all express routes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const config = require('./config/config');
 /* Import the Express library and it's supporting libraries*/
 const express = require('express');
 const cors = require('cors');
+const logging = require('./config/morgan');
 
 /* Import any routes we need */
 
@@ -12,6 +13,10 @@ const app = express();
 
 /* Assign the supporting libraries to the express app */
 app.use(cors());
+
+/* Setup the logging for the app */
+app.use(logging.accessRotate);
+app.use(logging.consoleError);
 
 /* Assign routes to the express app */
 
