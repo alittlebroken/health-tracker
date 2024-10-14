50f2c9311158490e23fad60325820bdfbf9c5211
Paul Lockyer <plockyer@googlemail.com>
2024-10-14 18:01:54 +0100

    added DB env vars to config

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,10 @@ require('dotenv').config()
 
 module.exports = {
     APP_PORT: process.env.APP_PORT,
-    LOG_ACCESS_LOC: process.env.LOG_ACCESS_LOC
+    LOG_ACCESS_LOC: process.env.LOG_ACCESS_LOC,
+    DB_PASS: process.env.DB_PASS,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+    DB_HOST: process.env.DB_HOST
 }
