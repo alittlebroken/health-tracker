858c65db8aaba7b6a81afe65325241aee24151ee
Paul Lockyer <plockyer@googlemail.com>
2024-10-15 07:23:30 +0100

    modified to assign env to const vars and then export those

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
 
 /* Import the correct environmental vars needed */
+/* Base application config */
+const APP_PORT = process.env.APP_PORT;
+
+/* Logging config */
+const LOG_ACCESS_LOC = process.env.LOG_ACCESS_LOC;
+
+/* Database connection information */
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 module.exports = {
-    APP_PORT: process.env.APP_PORT,
-    LOG_ACCESS_LOC: process.env.LOG_ACCESS_LOC,
-    DB_PASS: process.env.DB_PASS,
-    DB_USER: process.env.DB_USER,
-    DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT,
-    DB_HOST: process.env.DB_HOST
+    APP_PORT,
+    LOG_ACCESS_LOC,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    DB_HOST,
+    DB_PORT
 }
