f8b8af8efccf77ab16ca2784810b2df186baab98
Paul Lockyer <plockyer@googlemail.com>
2024-10-16 08:58:45 +0100

    added the auth routes to the server

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
 /* Import any routes we need */
+const authRoutes = require('./routes/Auth/Auth.routes');
 
 /* Create an instance of the app */
 const app = express();
@@ -26,7 +27,7 @@ app.use(logging.consoleError);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 /* Assign routes to the express app */
-
+app.use(authRoutes);
 
 /* export the app so it can be used in tests and the main app in the root directory */
 module.exports = app;
