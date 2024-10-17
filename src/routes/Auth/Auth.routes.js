3743709c04279a47cac20ea9f8de722a4b59cd33
Paul Lockyer <plockyer@googlemail.com>
2024-10-17 09:07:16 +0100

    fleshed out the swagger do for the route

--- src/routes/Auth/Auth.routes.js
+++ src/routes/Auth/Auth.routes.js
@@ -14,6 +14,45 @@ const controller = require('../../controllers/Auth/Auth.controller.js');
  *   post:
  *     summary: Register a new user
  *     description: Rgisters a user for use within the Health Tracker system
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   description: The user's email address
+ *                   example: user.new@textcompany.org
+ *                 password:
+ *                   type: string
+ *                   description: The password the user wishes to have for thier account
+ *                   example: Ex4mpl3P455w0RD!
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: integer
+ *                   description: The status code of the response
+ *                   example: 201
+ *                 state:
+ *                   type: string
+ *                   description: Wether the request was a success or failure
+ *                   example: ok
+ *                 message:
+ *                   type: string
+ *                   description: Details the request state with more information
+ *                   example: User registered
+ *                 results:
+ *                   type: array
+ *                   description: A list of the users registered
+ *                   example: [{ id: 1, email: 'user.new@textcompany.org' }]
  */
 router.post('/register', controller.Register);
 
